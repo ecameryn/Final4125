b748e4a17e1534c43f078770054203e8be5bd21b
Cameryn Holstick <cholstick0104@gmail.com>
2016-12-10 13:00:37 -0500

    Everything working but Model

---
+++ Final4125/Controllers/DTController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final4125.Controllers
+{
+    public class DTController : Controller
+    {
+        // GET: DT
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String Today()
+        {
+            DateTime d = DateTime.Now;
+            //string date = HttpServerUtility.HtmlEncode(Convert.ToString(d));
+
+            return "Right now is " + d;
+        }
+    }
+}
